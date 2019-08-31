f76a3f0aac6f804adce50f65948e53b1fca37531
alexey-radko <alexej.radko@googlemail.com>
2019-08-31 18:12:25 +0200

    added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-
+    
 
 
     <div id="app" class='container mt-200'> 
-    <h1>My Shop</h1>
+    <navbar :cards="cards"
+    @toggle="toggleSliderStatus"
+    @delete="deleteItem"
+    :cartQty="cartQty"
+    :cartTotal="cartTotal"
+    
+    
+    > </navbar> 
+    <h1 class ="mt-5 mb-4">My Shop</h1>
     <p class="animated fadeInRight"> See what we have for you!</p>
     
     <price-slider :sliderStatus="sliderStatus" :maximum.sync="maximum" > </price-slider>
@@ -14,6 +22,7 @@
 </template>
 
 <script>
+import Navbar from "./components/Navbar"
 import Price from "./components/Price"
 import ProductList from "./components/Products"
 import PriceSlider from "./components/Price-Slider"
@@ -22,24 +31,54 @@ export default {
   components:{
     Price,
     ProductList,
-    PriceSlider
+    PriceSlider,
+    Navbar
   },
   data () {
     return {
       maximum:100,
       products: null,
       cards:[],
-      sliderStatus:true
+      sliderStatus:false
     }
   },
-  mounted: function() {
+  mounted:  function() {
     fetch("https://hplussport.com/api/products/order/price")
       .then(response => response.json())
       .then(data => {
         this.products = data;
       });
   },
+  computed:{
+    cartTotal: function(){
+            var sum = 0;
+            for(let key in this.cards){
+                
+                sum += (this.cards[key].product.price * this.cards[key].qty);
+            }
+            return sum;
+        },
+        cartQty: function(){
+            var qty = 0;
+            for(let key in this.cards){
+                
+                qty += (this.cards[key].qty);
+            }
+            return qty;
+        }
+  },
   methods:{
+     deleteItem: function(index){
+            console.log("pushed");
+            if(this.cards[index].qty>1){
+                this.cards[index].qty--; 
+            } else {
+                this.cards.splice(index, 1);
+            }
+        },
+    toggleSliderStatus: function(){
+      this.sliderStatus = !this.sliderStatus
+    },
     addItem: function(product){
       var productIndex;
       var existing = this.cards.filter(function(item, index){
---
+++ src/components/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav class="navbar navbar-expand-sm bg-light fixed-top">
+    <ul class="nav justify-content-end">
+      <li class="nav-item">
+        <a class="nav-link btn" href="#" v-on:click="showList()">Go back</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link btn" href="#" @click="$emit('toggle')"  >Filtern</a>
+      </li>
+      <li class="nav-item">
+        <div class="dropdown">
+          <button type="button" class="btn dropdown-toggle" data-toggle="dropdown">
+            Items:
+            <span class="badge badge-light">{{cartQty}}</span>
+            Summe:
+            <span class="badge badge-light">
+              <price
+                :value="Number(cartTotal)"
+                :prefix="'&euro;'"
+                :precision="2"
+                :conversion="0.89"
+              ></price>
+            </span>
+          </button>
+          <div class="dropdown-menu">
+            <div class="dropdown-item" v-for="(index, item) in cards" :key="item.id">
+              <span class="badge badge-light">{{index.qty}}</span>
+              {{index.product.name}}
+              <b>{{ Number(index.product.price) * index.qty }}</b>
+              <a
+                href="#"
+                v-on:click.stop="$emit('delete', item)"
+                class="badge badge-danger text-white mg-10"
+              >–</a>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+
+<script>
+import Price from "./Price"
+export default {
+    name: "navbar",
+    components:{
+        Price
+    },
+    props:[
+        'cartQty', 'cards', 'cartTotal'
+    ]
+};
+</script>
+
+<style>
+</style>template
